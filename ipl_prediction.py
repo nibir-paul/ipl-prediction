e54017caead043af93a40b45624251633df50ccc
Paul <NP086260@cerner.net>
2020-12-24 10:12:33 +0530

    Addition of the model building phase

--- ipl_prediction.py
+++ ipl_prediction.py
@@ -1,53 +1,64 @@
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import KFold
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn import metrics
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
 
 matches = pd.read_csv("input/matches.csv")
 
-#Getting the info of the dataset
+# Generating the info of the dataset to check for anomalies
 print(matches.info())
 print("-------------------------------------------------------")
 
-#Check for columns with missing values
-for i in matches.columns:
-    print("Checking null values for ", i, " column")
-    print(matches[pd.isnull(matches[i])])
+# This will give a general idea of the missing values in each column
+for column in matches.columns:
+    print("Checking null values for ", column, " column")
+    print(matches[pd.isnull(matches[column])])
     print("-------------------------------------------------------")
 
-#Replacing missing values in winners column with draw
+# Since winners can't be null so replacing those null values with Draw
 matches['winner'].fillna('Draw', inplace=True)
 
-#Replacing missing values in cities column with Dubai
-matches['city'].fillna('Dubai',inplace=True)
+# Since these matches were held in Dubai, replacing the missing values of city with Dubai
+matches['city'].fillna('Dubai', inplace=True)
 
-#Encoding of the team names
-matches.replace(['Mumbai Indians','Kolkata Knight Riders','Royal Challengers Bangalore','Deccan Chargers','Chennai Super Kings',
-                 'Rajasthan Royals','Delhi Daredevils','Gujarat Lions','Kings XI Punjab',
-                 'Sunrisers Hyderabad','Rising Pune Supergiants','Rising Pune Supergiant','Kochi Tuskers Kerala','Pune Warriors']
-                ,['MI','KKR','RCB','DC','CSK','RR','DD','GL','KXIP','SRH','RPS','RPS','KTK','PW'],inplace=True)
-encode = {'team1': {'MI':1,'KKR':2,'RCB':3,'DC':4,'CSK':5,'RR':6,'DD':7,'GL':8,'KXIP':9,'SRH':10,'RPS':11,'KTK':12,'PW':13},
-          'team2': {'MI':1,'KKR':2,'RCB':3,'DC':4,'CSK':5,'RR':6,'DD':7,'GL':8,'KXIP':9,'SRH':10,'RPS':11,'KTK':12,'PW':13},
-          'toss_winner': {'MI':1,'KKR':2,'RCB':3,'DC':4,'CSK':5,'RR':6,'DD':7,'GL':8,'KXIP':9,'SRH':10,'RPS':11,'KTK':12,'PW':13},
-          'winner': {'MI':1,'KKR':2,'RCB':3,'DC':4,'CSK':5,'RR':6,'DD':7,'GL':8,'KXIP':9,'SRH':10,'RPS':11,'KTK':12,'PW':13,'Draw':14}}
-matches.replace(encode, inplace=True)
+# Only these features matter for our model
+features = matches[['team1','team2','city','toss_decision','toss_winner','venue']]
+target = matches[['winner']]
+print(features.head())
+print(target.head())
 
-#Descending order of teams winning the tosses and Descending order of wins by the teams
-dicVal = encode['winner']
-matches = matches[['team1','team2','city','toss_decision','toss_winner','venue','winner']]
-temp1 = matches['toss_winner'].value_counts(sort=True)
-temp2 = matches['winner'].value_counts(sort=True)
-print(matches.head())
-print("-------------------------------------------------------")
-print('No of toss winners by each team')
-for idx, val in temp1.iteritems():
-   print('{} -> {}'.format(list(dicVal.keys())[list(dicVal.values()).index(idx)],val))
-print("-------------------------------------------------------")
-print('No of match winners by each team')
-for idx, val in temp2.iteritems():
-   print('{} -> {}'.format(list(dicVal.keys())[list(dicVal.values()).index(idx)],val))
+# One final check so that there are no missing values
+print(features.apply(lambda x: sum(x.isnull()),axis=0))
 
-#encoding of the categorical variables
-var_mod = ['city','toss_decision','venue']
-le = LabelEncoder()
-for i in var_mod:
-    matches[i] = le.fit_transform(matches[i])
-print(matches.dtypes)
+#Splitting the data into training and testing data and scaling it
+X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=0,shuffle=True)
+
+#Logistic Regression
+logisticregression = LogisticRegression()
+logisticregression.fit(X_train, y_train)
+y_pred = logisticregression.predict(X_test)
+print('Accuracy of Logistic Regression Classifier on test set: {:.4f}'.format(logisticregression.score(X_test, y_test)))
+
+#Decision Tree Classifier
+decisiontree = DecisionTreeClassifier()
+decisiontree.fit(X_train,y_train)
+y_pred = decisiontree.predict(X_test)
+print('Accuracy of Decision Tree Classifier on test set: {:.4f}'.format(decisiontree.score(X_test, y_test)))
+
+#SVM
+svm = SVC()
+svm.fit(X_train,y_train)
+y_pred = svm.predict(X_test)
+print('Accuracy of SVM Classifier on test set: {:.4f}'.format(svm.score(X_test, y_test)))
+
+#Random Forest Classifier
+randomForest = RandomForestClassifier(n_estimators=100)
+randomForest.fit(X_train,y_train)
+y_pred = randomForest.predict(X_test)
+print('Accuracy of Random Forest Classifier on test set: {:.4f}'.format(randomForest.score(X_test, y_test)))
